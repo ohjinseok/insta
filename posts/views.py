6677f603e89f32ef0a991b9ae0fae443b89763ff
JinSeok Oh <ohjinseok1113@gmail.com>
2019-04-11 06:11:43 +0000

    Post 삭제 기능 추가

--- posts/views.py
+++ posts/views.py
@@ -9,9 +9,32 @@ def create(request):
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('posts:create')
+            return redirect('posts:list')
         pass
     else: # GET
         # post를 작성하는 form을 보여줌
         form = PostForm()
         return render(request, 'posts/create.html', {'form': form})
+        
+        
+def list(request):
+    # 모든 Post를 보여줌
+    Posts = Post.objects.all()
+    return render(request, 'posts/list.html', {'posts': Posts})
+
+
+def delete(request, post_id):
+    Post.objects.get(id=post_id).delete()
+    return redirect('posts:list')
+
+
+def update(request, post_id):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:list')
+    else:
+        post = Post.objects.get(id=post_id)
+        form = PostForm()
+        return render(request, 'posts/update.html', {'post': post, 'form': form})

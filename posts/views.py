9c12f2ca5418dcf0bda1268fb076ec658277e748
JinSeok Oh <ohjinseok1113@gmail.com>
2019-04-11 08:25:28 +0000

    User 모델과 Post 모델을 1:N으로 연결

--- posts/views.py
+++ posts/views.py
@@ -26,12 +26,18 @@ def list(request):
 
 def delete(request, post_id):
     post = get_object_or_404(Post, id=post_id)
+    
+    if post.user != request.user:
+        return redirect('posts:list')
+        
     post.delete()
     return redirect('posts:list')
 
 
 def update(request, post_id):
     post = get_object_or_404(Post, id=post_id)
+    if post.user != request.user:
+        return redirect('posts:list')
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
